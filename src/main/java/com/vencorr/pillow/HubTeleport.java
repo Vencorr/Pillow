3d5039396b1416a29e13146fd5f82107a2ffc7c5
Nicholas George <vencorr@zoho.com>
2019-11-16 02:13:31 -0600

    Allowed HubTP to only work in overworld

--- src/main/java/com/vencorr/pillow/HubTeleport.java
+++ src/main/java/com/vencorr/pillow/HubTeleport.java
@@ -1,5 +1,7 @@
 package com.vencorr.pillow;
 
+import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,7 +12,12 @@ public class HubTeleport implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            return new TeleportProgress(player, player.getWorld().getSpawnLocation()).runn >= 0;
+            if (player.getWorld().getEnvironment() == World.Environment.NORMAL) return new TeleportProgress(player, player.getWorld().getSpawnLocation()).runn >= 0;
+            else {
+                String errMes = ChatColor.RED + "Pillow.HubTP was unable to begin teleport. This could be due to: \n" +
+                        ChatColor.GOLD + ChatColor.ITALIC + " - You are not in the overworld.\n";
+                player.sendMessage(errMes);
+            }
         } else {
             sender.sendMessage("Sender must be a player!");
         }
